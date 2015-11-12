7d742017ba2e159d4961dc1821776009e004bb90
huanghuibao <huanghuibao@allwinnertech.com>
2015-11-12 10:47:57 +0800

    rm BuildLog.htm and project ilk.
    Signed-off-by: huanghuibao <huanghuibao@allwinnertech.com>

--- fisheyeimage/main.cpp
+++ fisheyeimage/main.cpp
@@ -87,61 +87,109 @@ int main(int argc, char **argv)
 
 	correctParameters params;
 
-	int x = 342;
-	int y = 257;
-	int r = 332;
+	int x = 450;
+	int y = 360;
+	int r = 360;
 
 	namedWindow("ori",1);
 	imshow("ori", image);
-	namedWindow("img0",1);
+	/*namedWindow("img0",1);
 	namedWindow("img1",1);
 	namedWindow("img2",1);
 	namedWindow("img3",1);
-	namedWindow("img4",1);
+	namedWindow("img4",1);*/
 
+	params.imgOrg = image;
+	params.center = Point2i(x, y);
+	params.radius = r;
+	params.w_longtitude = PI / 2;
+	params.w_latitude = PI / 2;
+	params.distMap = LATITUDE_LONGTITUDE;
+	params.theta_left = 0;
+	params.phi_up = 0;
+	params.camerFieldAngle = findCircleParameter::FOV;
+	params.camProjMode = EQUIDISTANCE;
+	params.typeOfCorrect = Reverse;
+
+	camMode c = EQUIDISTANCE;
+	distMapMode m = PERSPECTIVE;
+	CorrectType t = Reverse;
 
 	int key = 0;
 	while (key != 27) {
 
 		switch (key) {
-			case 'a': break;
-			case 'd': break;
-			case 'w': break;
-			case 's': break;
-			case '1': break;
-			case '3': break;
-			case '4': break;
-			case '6': break;
-			case '7': break;
-			case '9': break;
+			case 'a': params.w_longtitude -= .1f; break;
+			case 'd': params.w_longtitude += .1f; break;
+			case 'w': params.w_latitude += .1f; break;
+			case 's': params.w_latitude -= .1f; break;
+			case 'c': if (c==STEREOGRAPHIC) c=EQUIDISTANCE; else if(c==EQUIDISTANCE) c=EQUISOLID; else if (c==EQUISOLID) c=ORTHOGONAL; else if (c==ORTHOGONAL) c=STEREOGRAPHIC; break;
+			case 'm': if (m==PERSPECTIVE) m=LATITUDE_LONGTITUDE; else if (m==LATITUDE_LONGTITUDE) m=PERSPECTIVE; break;
+			case 't': if (t==Reverse) t=Forward; else if (t==Forward) t=Reverse; break;
+			case 'u': params.theta_left -= .1f; break;
+			case 'i': params.theta_left += .1f; break;
+			case 'o': params.phi_up -= .1f; break;
+			case 'p': params.phi_up += .1f; break;
+			case 'k': params.camerFieldAngle -= .1f; break;
+			case 'l': params.camerFieldAngle += .1f; break;
+			case '1': x -= 1; break;
+			case '3': x += 1; break;
+			case '4': y -= 1; break;
+			case '6': y += 1; break;
+			case '7': r -= 1; break;
+			case '9': r += 1; break;
 			default: break;
 		}
-
-		params.imgOrg = image;
+		cout << "xyr=" << x << "," << y << "," << r << ",w_l=(" << params.w_longtitude << "," << params.w_latitude << "),cm=" << c << ",mm=" << m << ",t=" << t << ",theta,phi=" << params.theta_left << "," << params.phi_up << ",cangle=" << params.camerFieldAngle << endl;
 		params.center = Point2i(x, y);
 		params.radius = r;
-		params.w_longtitude = PI / 2;
-		params.w_latitude = PI / 2;
-		params.distMap = LATITUDE_LONGTITUDE;
-		params.theta_left = 0;
-		params.phi_up = 0;
-		params.camerFieldAngle = findCircleParameter::FOV;
-		params.camProjMode = EQUIDISTANCE;
-		params.typeOfCorrect = Reverse;
+		params.distMap = m;
+		params.camProjMode = c;
 
 		corrector adjuster;
+#if 0
 		Mat img0 = adjuster.getCorrectImage(params, corrector::correctMethod::LONG_LAT_MAP_REVERSE_FORWARD);
+		namedWindow("img0",1);
 		imshow("img0", img0);
+#endif
+#if 1
 		Mat img1 = adjuster.getCorrectImage(params, corrector::correctMethod::PERSPECTIVE_LONG_LAT_MAP_CAMERA_LEN_MODEL);
+		namedWindow("img1",1);
 		imshow("img1", img1);
+		imwrite("defish1.jpg", img1);
+#endif
+#if 0
 		Mat img2 = adjuster.getCorrectImage(params, corrector::correctMethod::PERSPECTIVE_LONG_LAT_MAP_CAMERA_LEN_MODEL_REVERSE_W_HALF_PI);
+		namedWindow("img2",1);
 		imshow("img2", img2);
+		//imwrite("defish2.jpg", img2);
+#endif
+#if 0
 		Mat img3 = adjuster.getCorrectImage(params, corrector::correctMethod::PERSPECTIVE_LONG_LAT_MAP_CAMERA_LEN_MODEL_REVERSE_W_VARIABLE);
+		namedWindow("img3",1);
 		imshow("img3", img3);
 		Mat img4 = adjuster.getCorrectImage(params, corrector::correctMethod::PERSPECTIVE_LONG_LAT_MAP_CAMERA_LEN_MODEL_FORWORD_W_VARIABLE);
+		namedWindow("img4",1);
 		imshow("img4", img4);
+#endif
 
 		key = cvWaitKey(0);
 	}
+	return 0;
+}
+
+int main_x(int argc, char **argv)
+{
+	vector<Mat> images1,images2;
+	for (int i = 1;i < argc;i++)
+	{
+		if (i < argc - 1)
+			images1.push_back(imread(argv[i]));
+		else
+			images2.push_back(imread(argv[i]));
+	}
+	imagesStitcher composer;
+	images2.insert(images2.begin(),composer.stitchImages(images1));	
+	composer.showPanorama();
 	return 0;
 }
